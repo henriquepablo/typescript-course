b05347a7f4b71e17ed9b820efd9ba6a3fb480f4a
henriquepablo <pablo269704@gmail.com>
2024-05-22 14:44:16 -0300

    doc: type union

--- dia3/code/index.ts
+++ dia3/code/index.ts
@@ -1,3 +1,25 @@
-let value;
-value = 10;
-value = 'Lacerda';
+// union type permite aceitar mais de um tipo. Exemplo:
+/*
+    function userInput(a:number | string, b:number | string) {
+        let result;
+        if (typeof a === 'string' &&  typeof b === 'string') {
+            result = a + b;
+        } 
+        else {
+            result = a.toString() + b.toString();
+        }
+        return result;
+    }
+*/
+
+
+function userInput(a:number | string, b:number | string) {
+    let result;
+    if (typeof a === 'string' &&  typeof b === 'string') {
+        result = a + b;
+    } 
+    else {
+        result = a.toString() + b.toString();
+    }
+    return result;
+}
